bc99a299427f384274ffdb4cb02856c5e6d4f877
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-06-16 22:50:11 +0900

    qorwns_2294.py

---
+++ DP/qorwns_2294.py
@@ -0,0 +1,15 @@
+n,k=map(int,input().split())
+coins=[]
+for _ in range(n):
+    coins.append(int(input()))
+dp=[10001]*(k+1)
+dp[0]=0
+
+for i in coins:
+    for j in range(i,k+1):
+        dp[j]=min(dp[j],dp[j-i]+1)
+        
+if dp[k]==10001:
+    print(-1)
+else:
+    print(dp[k])
