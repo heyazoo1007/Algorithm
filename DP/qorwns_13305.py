965220fbc6f4a4407e4e8e7599c06ebe6b7da9ad
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-06-28 21:48:04 +0900

    qorwns_13305.py

---
+++ DP/qorwns_13305.py
@@ -0,0 +1,13 @@
+n=int(input())
+distance = list(map(int,input().split()))
+oil = list(map(int,input().split())) 
+
+final = oil[0]
+result=0
+
+for i in range(n-1):
+    if oil[i]<final:
+        final=oil[i]
+    result+=distance[i]*final
+       	
+print(result)
