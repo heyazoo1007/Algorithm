c9c1a1b635e12e11192c9689d212bfc606775311
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-07-11 18:26:31 +0900

    qorwns_2212.py

---
+++ Greedy/qorwns_2212.py
@@ -0,0 +1,15 @@
+n=int(input())
+k=int(input())
+data=list(map(int,input().split()))
+data.sort()
+
+distance=[]
+if k>=n:
+    print(0)
+else:
+    for i in range(1,n):
+        distance.append(data[i]-data[i-1])
+    distance.sort(reverse=True)
+    for _ in range(k-1):
+        distance.pop(0)
+    print(sum(distance))
