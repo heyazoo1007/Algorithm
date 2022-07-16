60b6dffffd43f12e06fa0de6f4b700b9cdb378bc
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-07-16 18:12:58 +0900

    qorwns_11051.py

---
+++ Greedy/qorwns_11051.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+for _ in range(int(input())):
+    n=int(input())
+    data=list(map(int,input().split()))
+    result=[0]*n
+    result[-1]=data[-1]
+    
+    for i in range(n-2,-1,-1):
+        if data[i]>data[i+1] and data[i]>result[i+1]:
+            result[i]=data[i]
+        else:
+            result[i]=result[i+1]
+            
+    answer=0
+    for i in range(n):
+        answer+=result[i]-data[i]
+    print(answer)
