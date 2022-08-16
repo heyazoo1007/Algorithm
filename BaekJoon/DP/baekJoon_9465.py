f97050517d6f649689cd90a59e4e3118a8dd6599
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-16 18:15:44 +0900

    baekJoon_9465.py

--- BaekJoon/DP/baekJoon_9465.py
+++ BaekJoon/DP/baekJoon_9465.py
@@ -4,6 +4,7 @@ for _ in range(t):
     data=[]
     for _ in range(2):
         data.append(list(map(int,input().split())))
+        
     for j in range(1,n):
         if j==1:
             data[0][j]+=data[1][j-1]
@@ -11,4 +12,5 @@ for _ in range(t):
         else:
             data[0][j]+=max(data[1][j-1],data[1][j-2])
             data[1][j]+=max(data[0][j-1],data[0][j-2])
+            
     print(max(data[0][n-1],data[1][n-1]))
