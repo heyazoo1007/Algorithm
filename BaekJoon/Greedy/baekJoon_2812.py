4ad7b079c323c2cb142ed6c5ba44d359d074b35f
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 21:35:29 +0900

    baekJoon_2812.py

--- BaekJoon/Greedy/baekJoon_2812.py
+++ BaekJoon/Greedy/baekJoon_2812.py
@@ -1,16 +1,16 @@
-N,K=map(int,input().split())
-data=list(input())
-result=[]
-k=K
+N,K = map(int,input().split())
+data = list(input())
+result = []
+k = K
 
 for i in range(N):
 
     #뺄 수 있는 횟수가 남아있고, 더 큰 숫자가 오른쪽에 있고, 아직 가장 큰 숫자가 오지 않았을 때
-    while k>0 and result and result[-1]<data[i]:
+    while k>0 and result and result[-1] < data[i]:
     
         #result의 오른쪽 원소를 제거해야하므로
         result.pop()
-        k-=1
+        k -= 1
         
     result.append(data[i])
     
