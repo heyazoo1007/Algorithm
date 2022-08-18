e39b6cd97678928036b99f961eb0fe0cc5915248
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 21:36:57 +0900

    backJoon_2828.py

---
+++ BaekJoon/Greedy/backJoon_2828.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+j = int(input())
+apples = []
+for _ in range(j):
+    apples.append(int(input()))
+    
+answer = 0
+current = [1,m]
+if m == 1:
+    for i in range(1,j):
+        answer += abs(apples[i]-apples[i-1])
+    print(answer)
+else:
+    for i in range(j):
+        if apples[i] > current[1]:
+            answer += (apples[i]-current[1])
+            current[0] = apples[i]-(m-1)
+            current[1] = apples[i]
+        elif apples[i] < current[0]:
+            answer += (current[0]-apples[i])
+            current[0] = apples[i]
+            current[1] = apples[i]+(m-1)
+            
+    print(answer)
