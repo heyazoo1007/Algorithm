642a96c9493d581c1be1f20a34bbe8b1d0fa277a
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 20:28:08 +0900

    baekJoon_13305.py

--- BaekJoon/Greedy/baekJoon_13305.py
+++ BaekJoon/Greedy/baekJoon_13305.py
@@ -1,13 +1,13 @@
-n=int(input())
+n = int(input())
 distance = list(map(int,input().split()))
 oil = list(map(int,input().split())) 
 
 final = oil[0]
-result=0
+result = 0
 
 for i in range(n-1):
-    if oil[i]<final:
-        final=oil[i]
-    result+=distance[i]*final
+    if oil[i] < final:
+        final = oil[i]
+    result += distance[i]*final
        	
 print(result)
