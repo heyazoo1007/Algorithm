380e8bf4cabed57460d0fdfea433cbc090bb64c7
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 20:26:16 +0900

    baekJoon_1202.py

--- BaekJoon/Greedy/baekJoon_1202.py
+++ BaekJoon/Greedy/baekJoon_1202.py
@@ -8,11 +8,11 @@ for _ in range(n):
 for _ in range(k):
     bag.append(int(input()))
     
-jewel.sort()
+jewel.sort() 
 bag.sort()
 
-count=0
-answer=[]
+count = 0
+answer = []
 for i in bag:
     #하나의 가방에 담을 수 있는 보석 후보 모두 저장
     while jewel and i >= jewel[0][0]:
@@ -20,5 +20,5 @@ for i in bag:
         heapq.heappop(jewel)
     #보석 후보가 있다면 그 중에서 가장 작은 가격(-최대가격)을 넣는다 
     if answer:
-        count+=heapq.heappop(answer)
+        count += heapq.heappop(answer)
 print(-count)
