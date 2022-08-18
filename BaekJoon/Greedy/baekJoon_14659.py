c1f96e614161cb1802ecfa4eb52b855889a30b51
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 20:29:22 +0900

    baekJoon_14659.py

---
+++ BaekJoon/Greedy/baekJoon_14659.py
@@ -0,0 +1,17 @@
+n = int(input())
+data = list(map(int,input().split()))
+sequence = [0]*n
+
+for i in range(n):
+    j = i+1    
+    while True:
+        
+        if j >= n:
+            break
+        if data[j] >= data[i]:
+            break
+        else:
+            sequence[i]+=1
+        j += 1
+        
+print(max(sequence))
