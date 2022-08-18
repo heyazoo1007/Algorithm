2b5a32009b7eb4cbeafed8cd3754abc912ffc431
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 20:24:50 +0900

    baekJoon_11000.py

--- BaekJoon/Greedy/baekJoon_11000.py
+++ BaekJoon/Greedy/baekJoon_11000.py
@@ -1,14 +1,16 @@
 import heapq
 
-n=int(input())
-data=[]
+n = int(input())
+data = []
 for _ in range(n):
     data.append(list(map(int,input().split())))
+    
 data.sort()
 last=[]
 heapq.heappush(last, data[0][1])
+
 for i in range(1,n):
-    if data[i][0]< last[0]:
+    if data[i][0] < last[0]:
         heapq.heappush(last,data[i][1])
     else:
         heapq.heappop(last)
