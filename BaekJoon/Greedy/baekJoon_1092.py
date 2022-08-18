45028f503e0e380fe0f81c2296c1baf6ad68e6ca
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 20:23:35 +0900

    baekJoon_1092.py

--- BaekJoon/Greedy/baekJoon_1092.py
+++ BaekJoon/Greedy/baekJoon_1092.py
@@ -5,15 +5,15 @@ boxes=list(map(int,input().split()))
 
 cranes.sort(reverse=True)           
 boxes.sort(reverse=True)
-if cranes[0]<boxes[0]:
+if cranes[0] < boxes[0]:
     print(-1)
 else:
-    answer=0
-    while(len(boxes)>0):
-        answer+=1
+    answer = 0
+    while(len(boxes) > 0):
+        answer += 1
         for i in range(n):           
            for j in range(len(boxes)):
-               if cranes[i]>=boxes[j]:
+               if cranes[i] >= boxes[j]:
                    del boxes[j]
                    break
     print(answer)
