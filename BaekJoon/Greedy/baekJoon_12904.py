a116df4ac4db6617888d04ae0bdc729f93f3079e
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 20:26:57 +0900

    baekJoon_12904.py

--- BaekJoon/Greedy/baekJoon_12904.py
+++ BaekJoon/Greedy/baekJoon_12904.py
@@ -2,15 +2,15 @@ s=list(input())
 t=list(input())
 
 while True:
-    if len(t)==len(s) :
-        if t==s:
+    if len(t) == len(s) :
+        if t ==s :
             print(1)
             break
         else:
             print(0)
             break
-    if t[-1]=='A':
-        t=t[:-1]
-    elif t[-1]=='B':
-        t=t[:-1]
+    if t[-1] == 'A':
+        t = t[:-1]
+    elif t[-1] == 'B':
+        t = t[:-1]
         t.reverse()
