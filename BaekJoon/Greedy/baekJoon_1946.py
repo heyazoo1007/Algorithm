36b68aa6fb30dada18c3c48ea13a24153c33ab46
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-08-18 21:33:45 +0900

    baekJoon_1946.py

--- BaekJoon/Greedy/baekJoon_1946.py
+++ BaekJoon/Greedy/baekJoon_1946.py
@@ -1,15 +1,16 @@
 for _ in range(int(input())):
-    n=int(input())
-    data=[]
+    n = int(input())
+    data = []
     for _ in range(n):
         data.append(list(map(int,input().split())))
-    zero=sorted(data,key=lambda x: x[0])
+    zero = sorted(data,key=lambda x: x[0])
     #선발인원 수 
-    count=1
+    count = 1
     result = zero[0][1]
     
     for i in range(1,n):
         if zero[i][1] < result:
-            count+=1
-            result=zero[i][1]       
+            count += 1
+            result = zero[i][1]     
+            
     print(count)
