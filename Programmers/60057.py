8e2359783a9c0c4526f567b41c07d84e7592e4ef
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-09-07 19:17:25 +0900

    60057.py

---
+++ Programmers/60057.py
@@ -0,0 +1,21 @@
+def solution(s):
+    n = len(s)    
+    answer = 1001
+
+    for i in range(1, n + 1):
+        temp = s[ : i]
+        count = 1
+        result = ''
+        for j in range(i, n + i, i):
+            if temp == s[j : j + i] :
+                count += 1
+            else:
+                if count == 1:
+                    result += temp
+                else: 
+                    result += (str(count) + temp)
+                    count = 1                
+                temp = s[j : j + i]      
+        answer = min(answer, len(result))
+        
+    return answer
