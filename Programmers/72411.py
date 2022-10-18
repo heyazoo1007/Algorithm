2dcf2273f10aff8797787a86ffd8d98873caa35f
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2022-10-18 22:28:53 +0900

    72411.py

---
+++ Programmers/72411.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    for k in course:
+        candidates = []
+        for menu_li in orders:
+            for li in combinations(menu_li, k):
+                res = ''.join(sorted(li))
+                candidates.append(res)
+                
+        sorted_candidates = Counter(candidates).most_common()
+        print(sorted_candidates)
+        
+        for menu, cnt in sorted_candidates:
+            if cnt > 1 and cnt == sorted_candidates[0][1]:
+                answer.append(menu)
+        
+    return sorted(answer)
