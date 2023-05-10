b2b7ccc7d7554c9c115e65bbde6eb66162e68b55
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-05-10 15:52:40 +0900

    Update baekJoon_2644.py

--- BaekJoon/Search/baekJoon_2644.py
+++ BaekJoon/Search/baekJoon_2644.py
@@ -1,29 +1,27 @@
-from collections import deque 
-
 n = int(input())
-x, y = map(int,input().split())
+first, second = map(int, input().split()) 
 m = int(input())
-data = [[] for _ in range(n+1)]
-visit = [0]*(n+1)
 
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
-    a,b = map(int,input().split())
-    data[a].append(b)
-    data[b].append(a)
-    
-def bfs(x,y):
-    queue = deque()
-    queue.append(x)
-    while queue:
-        x = queue.popleft()
-        if x == y:
-            return visit[y]
-        for i in data[x]:
-            if not visit[i]:
-                queue.append(i)
-                visit[i] = visit[x]+1
-    return -1        
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a) # 여기 추가 안 해서 틀림
 
-print(bfs(min(x,y),max(x,y)))    
-
+queue = [[first, 0]]
+visit = [0] * (n + 1)
+visit[first] = 1
+answer = -1
+while queue:
+    person, level = queue.pop(0)
     
+    if person == second:
+        answer = level
+        break
+    
+    for each in graph[person]:
+        if not visit[each]:
+            visit[each] = 1
+            queue.append([each, level + 1])
+
+print(answer)
