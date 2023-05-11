d1a68b58e4cc9380aca09e6286138f866a73ea5c
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-05-11 14:44:58 +0900

    Update baekJoon_7562.py

--- BaekJoon/Search/baekJoon_7562.py
+++ BaekJoon/Search/baekJoon_7562.py
@@ -1,26 +1,29 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-direction=[[-2,1],[-1,2],[1,2],[2,1],[-2,-1],[-1,-2],[1,-2],[2,-1]]
-
-def bfs(a,b,c,d):
-    queue=deque()
-    queue.append((a,b))
-    while queue:
-        x,y=queue.popleft()
-        if x==c and y==d:
-            break        
-        for i in range(8):
-            nx,ny=x+direction[i][0], y+direction[i][1]
-            if 0<=nx<n and 0<=ny<n and not data[nx][ny]:
-                data[nx][ny]=data[x][y]+1
-                queue.append((nx,ny))
-    return data[c][d]
+directions = [[-2, -1], [-1, -2], [-2, 1], [-1, 2], [1, -2], [2, -1], [2, 1], [1, 2]]
 
 for _ in range(int(input())):
-    n=int(input())
-    a,b=map(int,input().split())
-    c,d=map(int,input().split())
-    data=[[0]*(n+1) for _ in range(n+1)]
-    data[a][b]=1
-    print(bfs(a,b,c,d)-1)
+    n = int(input())
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+    # [a, b] 에서 [c, d]로 몇 번 만에 가는지 
     
+    queue = deque()
+    queue.append([a, b])
+    visit = [[0] * n for _ in range(n)]
+    visit[a][b] = 1 
+    while queue:
+        x, y = queue.popleft()
+        if x == c and y == d:
+            break
+        
+        for i in range(8):
+            nx, ny = x + directions[i][0], y + directions[i][1]
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visit[nx][ny]:
+                    visit[nx][ny] = visit[x][y] + 1
+                    queue.append([nx, ny])
+                    
+    print(visit[c][d] - 1)
