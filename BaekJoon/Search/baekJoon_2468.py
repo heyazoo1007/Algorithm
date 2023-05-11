5a5872b80f99598bb924c3d846f34c3ab2aa3a6a
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-05-11 14:43:57 +0900

    Update baekJoon_2468.py

--- BaekJoon/Search/baekJoon_2468.py
+++ BaekJoon/Search/baekJoon_2468.py
@@ -1,35 +1,37 @@
-from collections import deque
+n = int(input())
 
-n=int(input())
-data=[]
-count=0
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-
+matrix = []
+max_height = 0
 for _ in range(n):
-    data.append(list(map(int,input().split())))
-    
-def bfs(x,y,height):
-    queue=deque()
-    queue.append((x,y))
+    arr = list(map(int, input().split()))
+    max_height = max(max_height, max(arr))
+    matrix.append(arr)
+
+def bfs(x, y, visit, matrix, rain):
+    queue = [[x, y]]
+    visit[x][y] = 1
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
     while queue:
-        x,y=queue.popleft()
+        x, y = queue.pop(0)
+        
         for i in range(4):
-            nx,ny=x+dx[i],y+dy[i]
-            if 0<=nx<n and 0<=ny<n:
-                if not visit[nx][ny] and data[nx][ny]>=height:
-                    visit[nx][ny]=1
-                    queue.append((nx,ny))
-                    
-answer=0                    
-for x in range(1,100):  
-    count=0
-    visit=[[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if not visit[i][j] and data[i][j]>=x:
-                count+=1
-                bfs(i,j,x)
-    answer=max(count,answer)     
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n :
+                if not visit[nx][ny] and matrix[nx][ny] > rain:
+                    visit[nx][ny] = 1
+                    queue.append([nx, ny])
+        
+    
+answer = 0   
+for rain in range(max_height + 1):
+    temp = 0 # rain 높이에도 잠기지 않는 영역 
+    visit = [[0] * n for _ in range(n)]
+    for x in range(n):
+        for y in range(n):
+            if not visit[x][y] and matrix[x][y] > rain:
+                bfs(x, y, visit, matrix, rain)
+                temp += 1                
+                
+    answer = max(answer, temp)    
     
 print(answer)
