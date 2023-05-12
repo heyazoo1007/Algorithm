7846fdc835f93b0ffa776f81cc5f4b2d859b0533
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-05-12 12:56:28 +0900

    Update baekJoon_2583.py

--- BaekJoon/Search/baekJoon_2583.py
+++ BaekJoon/Search/baekJoon_2583.py
@@ -1,43 +1,37 @@
-from collections import deque
+m, n, k = map(int, input().split())
+matrix = [[0] * n for _ in range(m)]
+visit = [[0] * n for _ in range(m)]
 
-m,n,k=map(int,input().split())
-data=[[0]*n for _ in range(m)]
-visit=[[0]*n for _ in range(m)]
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-result=[]
-def change(a,b,c,d):
-    for i in range(m-d,m-b):
-        for j in range(a,c):
-            data[i][j]=1
 for _ in range(k):
-    a,b,c,d=map(int,input().split())
-    change(a,b,c,d)
+    x1, y1, x2, y2 = map(int, input().split()) # 좌표 (x1, y1)은 (m - y1)행 x1열
 
-def bfs(x,y):
-    queue=deque()
-    queue.append((x,y))
-    global result
-    count=1
-    while queue:
-        x,y=queue.popleft()
+    for i in range(m - y2, m - y1):
+        for j in range(x1, x2):
+            matrix[i][j] = 1
+
+def bfs(x, y, visit, matrix):   
+    queue = [[x, y]]
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    count = 1
+    while queue: 
+        x, y = queue.pop(0)
+        visit[x][y] = 1
         for i in range(4):
-            nx,ny=x+dx[i],y+dy[i]
-            if 0<=nx<m and 0<=ny<n:
-                if not visit[nx][ny] and not data[nx][ny]:
-                    visit[nx][ny]=1
-                    count+=1 #너무 복잡하게 생각함 
-                    queue.append((nx,ny)) #bfs의 기본 중 기본을 빼먹음
-    result.append(count)
-    
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                if not visit[nx][ny] and matrix[nx][ny] == 0:
+                    count += 1
+                    visit[nx][ny] = 1
+                    queue.append([nx, ny])
+    return count    
+
+answer = []
 for i in range(m):
     for j in range(n):
-        if not visit[i][j] and not data[i][j]:
-            visit[i][j]=1
-            bfs(i,j)
+        if not visit[i][j] and matrix[i][j] == 0:
+            answer.append(bfs(i, j, visit, matrix))
             
-result.sort()
-print(len(result))
-for i in range(len(result)):
-    print(result[i], end =' ')
-                   
+answer.sort()
+print(len(answer))
+for i in range(len(answer)):
+    print(answer[i], end = ' ')
