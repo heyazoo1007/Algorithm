1067e24310d02f56a58ba83c35405507a67b041e
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-05-17 22:34:32 +0900

    Update baekJoon_7576.py

--- BaekJoon/Search/baekJoon_7576.py
+++ BaekJoon/Search/baekJoon_7576.py
@@ -1,42 +1,42 @@
 from collections import deque
 
-m,n=map(int,input().split())
-data=[]
-tomato=deque()
-count=0
-for i in range(n):
-    data.append(list(map(int,input().split())))
-    #처음 토마토의 위치 담기
-    for j in range(m):
-        if data[i][j]==1:
-            tomato.append((i,j))
-            
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-visit=[[0]*m for _ in range(n)]
-
 def bfs():
-    while tomato: 
-        a,b=tomato.popleft()
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    while queue : 
+        x, y = queue.popleft()
         for i in range(4):
-            na,nb=a+dx[i],b+dy[i]
-            if 0<=na<n and 0<=nb<m:
-                if not visit[na][nb] and data[na][nb]==0:
-                    visit[na][nb]=1
-                    data[na][nb]=data[a][b]+1
-                    tomato.append((na,nb))
-
-bfs()
-minNum,maxNum=10**9,0
-
-#마지막에는 전체 탐색하면서 0이 있으면 -1, 아니면 최소 일수 출력
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if not visit[nx][ny] and matrix[nx][ny] == 0:
+                    visit[nx][ny] = 1
+                    matrix[nx][ny] = matrix[x][y] + 1
+                    queue.append([nx, ny])
+                    
+        
+m, n = map(int, input().split())
+matrix = []
+queue = deque()
 for i in range(n):
-    if 0 in data[i]:
-        minNum=0
-    else: 
-        maxNum = max(maxNum, max(data[i]))
-    
-if minNum==0:
+    matrix.append(list(map(int, input().split())))
+    for j in range(m):
+        if matrix[i][j] == 1:
+            queue.append([i, j])
+
+
+visit = [[0] * m for _ in range(n)]
+bfs()
+            
+answer = 0
+flag = True
+for i in range(n):
+    if 0 in matrix[i]:
+        flag = False
+        break
+    else:
+        answer = max(answer, max(matrix[i]))
+        
+if flag == True:    
+    print(answer - 1)
+else:
     print(-1)
-else: 
-    print(maxNum-1)
+            
