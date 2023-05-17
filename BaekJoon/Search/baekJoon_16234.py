d9b1b7e2b47899e03d5dbdb226f1aa8c32a9835d
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-05-17 22:48:45 +0900

    Update baekJoon_16234.py

--- BaekJoon/Search/baekJoon_16234.py
+++ BaekJoon/Search/baekJoon_16234.py
@@ -1,55 +1,45 @@
-from collections import deque
-import sys 
-input=sys.stdin.readline
-
-n,l,r=map(int,input().split())
-data=[]
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-
-for _ in range(n):
-    data.append(list(map(int,input().split())))
-    
-def move(result):
-    num=0
-    for x,y in result:
-        num+=data[x][y]
-    answer=num//len(result)    
-    for x,y in result:
-        data[x][y]=answer
-
-def bfs(i,j):
-    queue=deque()
-    queue.append((i,j))
-    visit[i][j]=1
-    result=[]
-    result.append([i,j])
+def bfs(x, y, matrix, visit):
+    visit[x][y] = 1
+    result = [[x, y]]
+    queue = [[x, y]]
     while queue:
-        x,y=queue.popleft()
+        x, y = queue.pop(0)
         for i in range(4):
-            nx,ny=x+dx[i],y+dy[i]
-            if 0<=nx<n and 0<=ny<n and not visit[nx][ny]:
-                if l <= abs(data[x][y]-data[nx][ny]) <= r:
-                    visit[nx][ny]=1
-                    queue.append((nx,ny))
-                    result.append([nx,ny])
-    return result                    
-                    
-days=0
-while True:
-    visit=[[0]*n for _ in range(n)]
-    isTrue=False
-    for i in range(n):
-        for j in range(n):
-            if not visit[i][j]:
-                visit[i][j]=1
-                result=bfs(i,j)
-                if len(result)>1:
-                    isTrue=True
-                    move(result)
-    if not isTrue:
-        break
-    days+=1
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visit[nx][ny] and l <= abs(matrix[nx][ny] - matrix[x][y]) <= r:
+                    visit[nx][ny] = 1
+                    queue.append([nx, ny])
+                    result.append([nx, ny]) 
+    return result
     
-print(days)                
-            
+n, l, r = map(int, input().split())
+matrix = []
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+for _ in range(n):
+    matrix.append(list(map(int, input().split())))
+    
+answer = 0
+while True:
+    flag = False
+    visit = [[0] * n for _ in range(n)]
+    for x in range(n):
+        for y in range(n):
+            if not visit[x][y]:
+                result = bfs(x, y, matrix, visit)
+                # 인구 이동을 할 수 있을 때
+                if len(result) >= 2:
+                    flag = True
+                    total = 0
+                    for i, j in result:
+                        total += matrix[i][j]
+                    temp = total // len(result)
+                    for i, j in result:
+                        matrix[i][j] = temp
+                                       
+    if flag == False:
+        break
+        
+    answer += 1
+        
+print(answer)
