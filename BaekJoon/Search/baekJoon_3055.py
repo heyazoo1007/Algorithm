16386141452669fa4fd38b99fcf3a66158e43c52
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-05-25 12:33:56 +0900

    Update baekJoon_3055.py

--- BaekJoon/Search/baekJoon_3055.py
+++ BaekJoon/Search/baekJoon_3055.py
@@ -1,45 +1,47 @@
-from collections import deque
-
-r,c=map(int,input().split())
-data=[]
-visit=[[0]*c for _ in range(r)]
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-queue=deque()
-
-for i in range(r):
-    data.append(list(input()))
-    for j in range(c):
-        if data[i][j]=='S':
-            queue.append((i,j))
-        elif data[i][j]=='D':
-            Dx,Dy=i,j
+n, m = map(int, input().split())
+matrix = []
+a, b = 0, 0 # 고슴도치 위치 
+c, d = 0, 0 # 비버 소굴 위치
+queue = []
+visit = [[0] * m for _ in range(n)]
+for i in range(n):
+    matrix.append(list(input()))
+    for j in range(m):
+        if matrix[i][j] == 'D':
+            c, d = i, j
+        if matrix[i][j] == 'S':
+            a, b = i, j
+            visit[i][j] = 1
+        if matrix[i][j] == '*':
+            queue.append([i, j, 'water'])
+            visit[i][j] = -1
             
-for i in range(r):
-    for j in range(c):
-        if data[i][j]=='*':
-            queue.append((i,j))
-            
-#고슴도치가 S, 목적지가 D
-def bfs(Dx,Dy):
-    while queue:
-        x,y=queue.popleft()
-        #목적지에 고슴도치가 도착해있는 경우 
-        if data[Dx][Dy]=='S':
-            #이동한 최단거리 반환 
-            return visit[Dx][Dy]
-        for i in range(4):
-            nx,ny=x+dx[i],y+dy[i]
-            if 0<= nx < r and 0<= ny < c:
-                #고슴도치는 비어있거나, 굴인 경우에만 이동할 수 있다 
-                if data[x][y]=='S' and (data[nx][ny]=='.' or data[nx][ny]=='D'):
-                    data[nx][ny]='S'
-                    visit[nx][ny]=visit[x][y]+1
-                    queue.append((nx,ny))
-                #물은 비어있거나, 고슴도치인 경우에만 이동할 수 있다 
-                elif data[x][y]=='*' and (data[nx][ny]=='.' or data[nx][ny]=='S'):
-                    data[nx][ny]='*'
-                    queue.append((nx,ny))
-    return 'KAKTUS'
-
-print(bfs(Dx,Dy))
+queue.append([a, b, 'kaktus'])
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+answer = 0
+flag = False
+while queue:
+    x, y, kind = queue.pop(0)
+    
+    if x == c and y == d:
+        if kind == 'kaktus':
+            flag = True
+            answer = visit[x][y] - 1
+            break
+    
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            if not visit[nx][ny] and (matrix[nx][ny] == '.' or matrix[nx][ny] == 'D'):
+                if kind == 'kaktus':
+                    visit[nx][ny] = visit[x][y] + 1
+                    queue.append([nx, ny, 'kaktus'])
+                if kind == 'water':
+                    if matrix[nx][ny] == '.':
+                        visit[nx][ny] = -1
+                        queue.append([nx, ny, 'water'])
+                    
+if flag == False :
+    print('KAKTUS')
+else:
+    print(answer)
