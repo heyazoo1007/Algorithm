9689947800dc5ca143c3a86011ee91c4ded8940c
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-08-29 09:19:09 +0900

    Update baekJoon_1205.py

--- BaekJoon/Implementation/baekJoon_1205.py
+++ BaekJoon/Implementation/baekJoon_1205.py
@@ -1,18 +1,25 @@
 from collections import Counter
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline #이거 해줘야 EOFError 발생 안함
 
 n, score, minimum = map(int, input().split())
 
 scores = list(map(int, input().split()))
 scores.sort(reverse = True)
 
-flag = True
 answer = -1
- 
-if flag : 
+flag = True
+if n <= minimum:
     if n < minimum:
         scores.append(score)
+    elif n == minimum :
+        if score > scores[-1]:
+            scores.pop()
+            scores.append(score)
+        else:
+            flag = False
+    
+    if flag : 
         temp = Counter(scores)
         result = sorted(temp.items(), key = lambda x : x[0], reverse = True)
         rank = 1
@@ -22,18 +29,4 @@ if flag :
                 break
             rank += y
             
-    elif n == minimum : 
-        if score > scores[-1]:
-            scores.pop()
-            scores.append(score)
-            temp = Counter(scores)
-            result = sorted(temp.items(), key = lambda x : x[0], reverse = True)
-            rank = 1
-            for x, y in result:
-                if x == score:
-                    answer = rank
-                    break
-                rank += y
-        
 print(answer)
-   
