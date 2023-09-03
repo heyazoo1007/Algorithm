e97687ce0d1b856f8e29a5d336bf68357c998041
Yejin Kim <81020108+heyazoo1007@users.noreply.github.com>
2023-09-04 08:58:41 +0900

    Create baekJoon_1244.py

---
+++ BaekJoon/Implementation/baekJoon_1244.py
@@ -0,0 +1,34 @@
+def reverse(number):
+    return abs(number - 1)
+
+
+n = int(input())
+switches = [0] + list(map(int, input().split()))
+
+for _ in range(int(input())):
+    gender, number = map(int, input().split())
+    
+    # 남자면 number의 배수만 상태 변경
+    if gender == 1: 
+        for i in range(number, n + 1):
+            if i % number == 0:
+                switches[i] = reverse(switches[i])
+                
+    # 여자인 경우는 양쪽으로 가면서 스위치 타입이 같아야 함           
+    else: 
+        switches[number] = reverse(switches[number])
+        before_index = number - 1
+        after_index = number + 1
+        while before_index >= 1 and after_index <= n:
+            if switches[before_index] != switches[after_index]:
+                break
+            switches[before_index] = reverse(switches[before_index])
+            switches[after_index] = reverse(switches[after_index])
+            before_index -= 1
+            after_index += 1
+
+m = len(switches)
+for i in range(1, len(switches)):
+    print(switches[i], end = ' ')
+    if i % 20 == 0 :
+        print()
